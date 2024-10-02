1b31329e0ba8a2b71ad5da332162bebfce6d4b97
xiajiujiang02 <xiajiujiang02@meituan.com>
2024-10-02 23:51:25 +0800

    method v2

--- repeat.js
+++ repeat.js
@@ -9,6 +9,9 @@
   repeatFunc("hello world");  //会输出4次 hello world, 每次间隔3秒
  */
 
+/**
+ * 【方法一📌】利用 async/await 特性，同步写法
+ */
 function repeat(func, times = 1, wait = 0) {
   const sleep = (delay = 0) => new Promise((resolve) => setTimeout(resolve, delay))
 
@@ -24,6 +27,30 @@ function repeat(func, times = 1, wait = 0) {
   return callback
 }
 
-const repeatFunc = repeat(console.log, 4, 3000)
+// const repeatFunc = repeat(console.log, 4, 3000)
+// repeatFunc('hello jiangxtx', Date.now())
+// console.log('repeatFunc>>', repeatFunc, typeof repeatFunc)
+
+/**
+ * 【方法二📌】纯粹利用 setTimeout 异步实现
+ */
+function repeat_2(func, times = 1, wait = 0) {
+  let count = 0
+  return function (...args) {
+    const delayRun = (delay = wait) => {
+      if (count >= times) return
+
+      setTimeout(() => {
+        count++
+        func(...args)
+        delayRun()
+      }, delay)
+    }
+
+    // 第一次执行 无需延迟
+    delayRun(0)
+  }
+}
+
+const repeatFunc = repeat_2(console.log, 4, 3000)
 repeatFunc('hello jiangxtx', Date.now())
-console.log('repeatFunc>>', repeatFunc, typeof repeatFunc)
