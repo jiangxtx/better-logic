9b0b4b4f7e346f3c9a3c50f80bb6592879a9805b
xiajiujiang02 <xiajiujiang02@meituan.com>
2024-10-30 11:33:09 +0800

    增加方法

--- max-bracket-composition.js
+++ max-bracket-composition.js
@@ -22,3 +22,29 @@ function maxBracketList(n) {
 }
 
 console.log('maxBracketList: ', maxBracketList(1), maxBracketList(2), maxBracketList(3))
+
+/**
+ * 递归 ＋ 回溯
+ */
+function maxBracketList_v2(n) {
+  const result = []
+
+  function generate(str, open, close) {
+    if (str.length === n * 2) {
+      result.push(str)
+      return
+    }
+    if (open < n) {
+      generate(str + '(', open + 1, close)
+      // return ！这里不能有return，否则就不完备了！
+    }
+    if (close < open) {
+      generate(str + ')', open, close + 1)
+    }
+  }
+
+  generate('', 0, 0)
+  return result
+}
+
+console.log('maxBracketList_v2: ', maxBracketList_v2(1), maxBracketList_v2(2), maxBracketList_v2(3))
